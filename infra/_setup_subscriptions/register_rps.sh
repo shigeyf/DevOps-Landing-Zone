3227831cecc17184d658c69ada30a019e8c6fa5d
Shige Fukushima <shigeyf@outlook.com>
2026-04-16 11:00:41 +0000

    fix: Update Resource Provider script

--- infra/_setup_subscriptions/register_rps.sh
+++ infra/_setup_subscriptions/register_rps.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Define options
-OPTIONS="s:f:h"
-LONGOPTIONS="subscriptionId:,file:,help"
+OPTIONS="s:f:nh"
+LONGOPTIONS="subscriptionId:,file:,nowait,help"
 PARSED=$(getopt --options "$OPTIONS" --longoptions "$LONGOPTIONS" --name "$(basename "$0")" -- "$@")
 eval set -- "$PARSED"
 
 # Initializa parameters
 subscriptionId=
 resourceProvidersFile=./resource_providers.txt
+nowait=false
 
 if [[ $? -ne 0 ]]; then
   # If getopt fails, exit with an error
@@ -20,11 +21,13 @@ while true; do
   case "$1" in
     -s|--subscriptionId) subscriptionId=$2;        shift 2 ;;
     -f|--file)           resourceProvidersFile=$2; shift 2 ;;
+    -n|--nowait)         nowait=true; shift 1 ;;
     -h|--help)
       echo "Usage: $(basename "$0") [options]"
       echo "Options:"
       echo "  -s, --subscriptionId  Azure Subscription Id to register resource providers"
       echo "  -f, --file            A filename taht contains a list of Resource Providers to be registered"
+      echo "  -n, --nowait          No wait for Resource Providers registration"
       echo "  -h, --help            Show this help message"
       echo ""
       shift;
@@ -41,7 +44,11 @@ for rp in `cat ${resourceProvidersFile}`; do
   state=$(az provider show --namespace ${rp} --query "registrationState" -o tsv)
   if [ "$state" != "Registered" ]; then
     echo -n "Registering Resource Provider $rp... "
-    az provider register --namespace ${rp} --wait && echo "Registered" || echo "Failed to register!"
+    if [ "$nowait" != "true" ]; then
+      az provider register --namespace ${rp} --wait && echo "Registered" || echo "Failed to register!"
+    else
+      az provider register --namespace ${rp} && echo "(check later)"
+    fi
   else
     echo "Resource provider $rp is already registered."
   fi
